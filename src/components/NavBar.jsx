5e3e06ebf9b6ecb5ffd2cd0961c273352951ad1b
David Fragua <davidfragua@gmail.com>
2022-11-11 13:19:07 +0100

    log out hecho

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,37 +1,36 @@
-import {NavLink} from "react-router-dom"
-
+import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../context/auth.context";
 
 function NavBar() {
+  const { authenticaUser, isLoggedIn, setUser, setIsLoggedIn } =
+    useContext(AuthContext);
+
+  const handleLogout = () => {
+    localStorage.removeItem("authToken");
+    authenticaUser();
+  };
+
   return (
     <div>
+      {isLoggedIn === true ? (
+        <div>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/service-list">Lista de Servicios</NavLink>
 
-      <NavLink to="/">
-        Home
-      </NavLink>
+          <NavLink to="/profile">Mi perfil</NavLink>
 
-      <NavLink to="/signup">
-        Sign Up
-      </NavLink>
-
-      <NavLink to="/login">
-        Log In
-      </NavLink>
-
-      <NavLink to="/service-list">
-        Lista de Servicios
-      </NavLink>
-
-      <NavLink to="/profile">
-        Mi perfil
-      </NavLink>
-
-      <NavLink to="">
-        Log Out
-      </NavLink>
-         
-    
+          <NavLink ><button onClick={handleLogout}>Log Out</button></NavLink>
+        </div>
+      ) : (
+        <div>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/signup">Sign Up</NavLink>
+          <NavLink to="/login">Log In</NavLink>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
