66d17359f614f6f8d5a4132a652fc7761a0e0fc9
David Fragua <davidfragua@gmail.com>
2022-11-14 10:41:41 +0100

    edit review hecho

--- src/components/ReviewList.jsx
+++ src/components/ReviewList.jsx
@@ -1,78 +1,115 @@
-import {useState, useEffect} from 'react'
-import { useNavigate } from 'react-router-dom'
-import {getAllReviewsService, deleteReviewService} from "../service/review.services"
-
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  getAllReviewsService,
+  deleteReviewService,
+  getReviewDetailsService,
+  editReviewService,
+} from "../service/review.services";
 
 function ReviewList() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const [myReviews, setMyReviews] = useState([]);
+  const [isFetching, setIsFetching] = useState(true);
+  const [reviewInput, setReviewInput] = useState("");
+  const [ratingInput, setRatingInput] = useState("");
+  const [showFormEditReview, setShowFormEditReview] = useState(false);
 
-    const [ myReviews, setMyReviews] = useState([])
-    const [isFetching, setIsFetching] = useState(true)
-    
-    useEffect(()=>{
-        getReviewsData()
-    }, [])
+  useEffect(() => {
+    getReviewsData();
+    // getEditReview()
+  }, []);
 
-    const getReviewsData = async () =>{
-
-        try{
-
-            const response = await getAllReviewsService()
-            setMyReviews(response.data)
-            setIsFetching(false)
-
-        }catch(error){
-            console.log(error)
-        }
+  const getReviewsData = async () => {
+    try {
+      const response = await getAllReviewsService();
+      setMyReviews(response.data);
+      setIsFetching(false);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    if (isFetching === true) {
-        return <h3>Loading</h3>
-      }
- 
-      //!falta incluir id de la review y le onclick en el boton
-    // const handleDelete = async() =>{
-    //     try{
+  if (isFetching === true) {
+    return <h3>Loading</h3>;
+  }
+  //!falta incluir id de la review
+  const getEditReview = async () => {
+    try {
+      const response = await getReviewDetailsService();
+      setReviewInput(response.data.review);
+      setRatingInput(response.data.rating);
+    } catch (error) {
+      console.log(error);
+      navigate("/error");
+    }
+  };
 
-    //         await deleteReviewService()
+  const updateReview = (event) => setReviewInput(event.target.value);
+  const updateRating = (event) => setRatingInput(event.target.rating);
 
-    //     }catch(error){
-    //         navigate(error)        
-    //     }
-    // }
+  const handleUpdateReview = async (event) => {
+    event.preventDefault();
 
-    
+    try {
+      const updatedReview = {
+        review: reviewInput,
+        rating: ratingInput,
+      };
+      //!falta incluir id de la review
+      await editReviewService(updatedReview);
+      setShowFormEditReview(true);
+      navigate("/profile");
+    } catch (error) {
+      console.log(error);
+      navigate("/error");
+    }
+  };
+
+  //!falta incluir id de la review y el onClick en el boton
+  // const handleDelete = async() =>{
+  //     try{
+
+  //         await deleteReviewService()
+
+  //     }catch(error){
+  //         navigate(error)
+  //     }
+  // }
 
   return (
-
     <div>
-    
-    {myReviews.map((eachElement)=>{
-    
-
-        return(
-            <div>
-            <h5>{eachElement.reviewedService.map((eachItem)=>{
-                return eachItem.title
-            })}</h5>
+      {myReviews.map((eachElement) => {
+        return (
+          <div>
+            <h5>
+              {eachElement.reviewedService.map((eachItem) => {
+                return eachItem.title;
+              })}
+            </h5>
             <p>Reseña:{eachElement.review}</p>
             <p>Valoración:{eachElement.rating}</p>
-            
-            <button>Editar Reseña</button>
-            <button>
-            Borrar Reseña
-            </button>
+
+            {/* // form para editar reseña */}
+            {showFormEditReview === true ? 
+                <div>
+                  <label htmlFor="review">Reseña</label> 
+                  <input type="text" name="review/"></input>
+                  <label htmlFor="rating">Rating</label> 
+                  <input type="text" name="rating/"></input>
+                </div>
+              : null }
+
+            {/* //! incluir handleDelete y handleUpdateReview */}
+            <button onClick={handleUpdateReview}>Editar Reseña</button>
+            <button>Borrar Reseña</button>
             <br />
-                        
-            </div>
-
-        )
-
-    })}
-
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default ReviewList
+export default ReviewList;
