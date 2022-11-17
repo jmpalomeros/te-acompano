9ec2809e4c3c059667fa8a400b587f6755c3ea20
David Fragua <davidfragua@gmail.com>
2022-11-17 13:24:43 +0100

    estilos  service list

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,44 +1,45 @@
-import {useState} from 'react'
+import { useState } from "react";
+import Form from 'react-bootstrap/Form';
 
 function Search(props) {
-
-  const[searchInput, setSearchInput] = useState("")
-  const [searchTypeServiceInput, setSearchTypeServiceInput] = useState("")
+  const [searchInput, setSearchInput] = useState("");
+  const [searchTypeServiceInput, setSearchTypeServiceInput] = useState("");
 
   const handleServiceChange = (event) => {
+    setSearchInput(event.target.value);
+    props.list(event.target.value);
+  };
 
-    setSearchInput(event.target.value)
-    props.list(event.target.value)
-  }
-
-  const handleTypeServiceChange = (event) =>{
-
-    setSearchTypeServiceInput(event.target.value)
-    props.typeService(event.target.value)
-  }
-
-
-
+  const handleTypeServiceChange = (event) => {
+    setSearchTypeServiceInput(event.target.value);
+    props.typeService(event.target.value);
+  };
 
   return (
     <div>
-    
-      <label htmlFor="search">Buscar servicios disponibles </label>
-      <input value={searchInput} type="text" placeholder='Buscar servicios' onChange={handleServiceChange}/>
-      
-
-      
+      <div>
+      <label htmlFor="search">Buscar por título de servicio  </label>
+      <Form.Control
+        value={searchInput}
+        type="text"
+        placeholder="Buscar servicios"
+        onChange={handleServiceChange}
+    >
+      </Form.Control>
+      </div>
+      <br />
+      <div>
       <label htmlFor="typeService">Buscar por tipo de servicio </label>
-      <select name="typeService" onChange={handleTypeServiceChange}>
-            <option value="">Elige una opción</option>
-            <option value="Ocio">Ocio</option>
-            <option value="Ayuda">Ayuda</option>
-            <option value="Otros">Otros</option>
-            </select>
-      {/* <input value={searchInput} type="text" placeholder='Buscar servicios' onChange={handleChange}/> */}
-
+      <Form.Select aria-label="" name="typeService" onChange={handleTypeServiceChange}>
+        <option value="">Elige una opción</option>
+        <option value="Ocio">Ocio</option>
+        <option value="Ayuda">Ayuda</option>
+        <option value="Otros">Otros</option>
+      </Form.Select>
+      <br />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Search
+export default Search;
--- src/pages/ServiceList.jsx
+++ src/pages/ServiceList.jsx
@@ -3,13 +3,16 @@ import { useState, useEffect, useContext } from "react";
 import { getAllServicesService } from "../service/service.services";
 import { AuthContext } from "../context/auth.context";
 import Search from "../components/Search";
+import ListGroup from "react-bootstrap/ListGroup";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 function ServiceList() {
   const navigate = useNavigate();
-  const { user } = useContext(AuthContext)
+  const { user } = useContext(AuthContext);
   const [list, setList] = useState([]);
-  const [typeServiceList, setTypeServiceList] = useState()
-  const [cloneList, setCloneList] = useState([])
+  const [typeServiceList, setTypeServiceList] = useState();
+  const [cloneList, setCloneList] = useState([]);
   const [isFetching, setIsFetching] = useState(true);
 
   useEffect(() => {
@@ -19,7 +22,7 @@ function ServiceList() {
   const getListData = async () => {
     try {
       const response = await getAllServicesService();
-      setCloneList(response.data)
+      setCloneList(response.data);
       setList(response.data);
       setIsFetching(false);
     } catch (error) {
@@ -31,46 +34,58 @@ function ServiceList() {
     return <h3>Loading</h3>;
   }
 
-
   const filterList = (filterQuery) => {
-    
     const listadoFiltrado = list.filter((eachElem) => {
       return eachElem.title.includes(filterQuery);
     });
-    setCloneList(listadoFiltrado)
-    
+    setCloneList(listadoFiltrado);
   };
 
   const filterTypeServiceList = (filterQuery) => {
     const listadoFiltrado = list.filter((eachElem) => {
       return eachElem.typeService.includes(filterQuery);
     });
-    
-    setTypeServiceList(listadoFiltrado)
-    setCloneList(listadoFiltrado)
-    
+
+    setTypeServiceList(listadoFiltrado);
+    setCloneList(listadoFiltrado);
   };
 
-  console.log(typeServiceList)
+  console.log(typeServiceList);
 
   return (
     <div>
-      <h3>Lista de servicios ofrecidos</h3>
+      <h3>Buscador de servicios disponibles</h3>
 
-      <Search list={filterList} typeService={filterTypeServiceList}/>
-      
+      <Search list={filterList} typeService={filterTypeServiceList} />
 
+      <h3>Lista de servicios disponibles</h3>
+      <div className="cards-service-list">
       {cloneList.map((eachElement) => {
         return (
-          <div key={eachElement._id}>
-            {eachElement.acceptedServices === undefined ? (
-              <Link to={`/service/${eachElement._id}`}>
-                <p key={eachElement._id}>{eachElement.title}</p>
-              </Link>
-            ) : null}
+          <div>
+            <div key={eachElement._id}>
+              {eachElement.acceptedServices === undefined ? (
+                <Card className="text-center"  style={{ width: "30vw" }}>
+                  <Card.Header>
+                    Servicio:{" "}
+                    <Card.Title>
+                      <Link to={`/service/${eachElement._id}`}>
+                        <p key={eachElement._id}>{eachElement.title}</p>
+                      </Link>
+                    </Card.Title>
+                  </Card.Header>
+                  <Card.Body>
+                    <p> Descripción: {eachElement.description}</p>
+                    <p> Categoría: {eachElement.typeService}</p>
+                  </Card.Body>
+                </Card>
+              ) : null}
+              <br />
+            </div>
           </div>
         );
       })}
+    </div>
     </div>
   );
 }
