fbfb6fe8427182ea76a34f478cbb0af02119fa01
David Fragua <davidfragua@gmail.com>
2022-11-17 18:45:00 +0100

    textos de páginas de errores hechos

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Nav from "react-bootstrap/Nav";
 
 function Error() {
   return (
-    <div>Error</div>
+    <div>
+      <h4>Ha habido un error con esta página</h4>
+      <h5>Estamos trabajando en ello</h5>
+
+      Puedes visitar la <Nav.Link to="/">Home</Nav.Link>
+
+
+    </div>
   )
 }
 
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import Nav from "react-bootstrap/Nav";
 
 
 function NotFound() {
   return (
     <div>
-    
-    NotFound</div>
+    <h3>Upps, lo sentimos</h3>
+    <h4>la página que estás buscando no existe</h4>
+    <br />
+    Puedes visitar la <Nav.Link to="/">Home</Nav.Link>
+
+
+    </div>
   )
 }
 
