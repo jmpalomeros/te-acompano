28ae0197fb8c6dc5c81b1523499ccc6f0cc79a21
David Fragua <davidfragua@gmail.com>
2022-11-17 15:25:11 +0100

    estilos login añadido y bug footer arreglado

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,84 +1,84 @@
 import { useState } from "react";
-import { loginService } from "../service/auth.services"
-import { useNavigate } from "react-router-dom"
+import { loginService } from "../service/auth.services";
+import { useNavigate } from "react-router-dom";
 
-import { useContext } from "react"
+import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 function Login() {
-
-  const { authenticaUser } = useContext(AuthContext)   
+  const { authenticaUser } = useContext(AuthContext);
 
   // configuramos el uso de navigate
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("")
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleEmailChange = (e) => setEmail(e.target.value);
   const handlePasswordChange = (e) => setPassword(e.target.value);
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    
 
     // 1. recopilar las credenciales del usuario
     const userCredentials = {
       email: email,
-      password: password
-    }
+      password: password,
+    };
 
     try {
       // 2. contactar con el backend para validarlo
-      const response = await loginService(userCredentials)
-      
-      localStorage.setItem("authToken", response.data.authToken)
-   
+      const response = await loginService(userCredentials);
 
-      authenticaUser() 
+      localStorage.setItem("authToken", response.data.authToken);
 
-  
-      navigate("/")
-      //! redireccionar luego a /profile
+      authenticaUser();
 
+      navigate("/");
     } catch (error) {
       if (error.response && error.response.status === 400) {
-        setErrorMessage(error.response.data.errorMessage)
+        setErrorMessage(error.response.data.errorMessage);
       } else {
-        navigate("/error")
+        navigate("/error");
       }
     }
   };
 
   return (
-    <div>
+    <div id="form-signup">
+      <br />
+      <h4>Acceder</h4>
+      <br />
+      <Form onSubmit={handleLogin}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email:</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleEmailChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </Form.Group>
+        <br />
 
-      <h1>Log In</h1>
-
-      <form onSubmit={handleLogin}>
-        <label>Email:</label>
-        <input
-          type="email"
-          name="email"
-          value={email}
-          onChange={handleEmailChange}
-        />
-
-        <label>Password:</label>
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-
-        <button type="submit">Login</button>
+        <Button type="submit" variant="primary">
+          Acceder
+        </Button>
 
         {errorMessage !== "" && <p>{errorMessage}</p>}
-
-      </form>
-      
+      </Form>
     </div>
   );
 }
